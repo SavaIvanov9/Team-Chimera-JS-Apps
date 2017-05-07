463c2fcc79682d7f2fe2bab6fb13457cb93c4590
Todor Dinkov <fasttakerbg@yahoo.com>
2017-05-07 20:40:13 +0300

    Adding gameScreen
    +gameScreen

--- JustSnake/Client/js/system.config.js
+++ JustSnake/Client/js/system.config.js
@@ -1,20 +1,21 @@
 SystemJS.config({
- // tell SystemJS which transpiler to use
- transpiler: 'plugin-babel',
- // tell SystemJS where to look for the dependencies
- map: {
-  'plugin-babel': 'node_modules/systemjs-plugin-babel/plugin-babel.js',
-  'systemjs-babel-build': 'node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+    // tell SystemJS which transpiler to use
+    transpiler: 'plugin-babel',
+    // tell SystemJS where to look for the dependencies
+    map: {
+        'plugin-babel': 'node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': 'node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
 
-  // app logic scripts
-  'routeManager': 'js/routing/routeManager.js',
-  'contentLoader': 'js/routing/screens/contentLoader.js',
-  'screenSelector': 'js/routing/screens/screenSelector.js',
-  'requestManager': 'js/tools/requestManager.js',
-  'data': 'js/data/data.js',
-  'homeController': 'js/controllers/homeController.js',
+        // app logic scripts
+        'routeManager': 'js/routing/routeManager.js',
+        'contentLoader': 'js/routing/screens/contentLoader.js',
+        'screenSelector': 'js/routing/screens/screenSelector.js',
+        'requestManager': 'js/tools/requestManager.js',
+        'data': 'js/data/data.js',
+        'homeController': 'js/controllers/homeController.js',
 
-  // app start script
-  'core': 'js/core.js'
- }
+
+        // app start script
+        'core': 'js/core.js'
+    }
 });
